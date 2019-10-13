92bb0219fca020c0e3e90f177224f0990097a1f6
nisemono0 <dazzle.x00@gmail.com>
2019-10-13 19:13:20 +0300

    Added mpd-rpc on mpd launch

--- .config/mpd/launch.sh
+++ .config/mpd/launch.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+if ! pgrep -x "mpd"
+then
+	[ ! -s ~/.config/mpd/pid ] && mpd
+fi
 
-[ ! -s ~/.config/mpd/pid ] && mpd
+
+if ! pgrep -x "mpd-rpc" > /dev/null
+then
+	mpd-rpc -h=/home/nise/.config/mpd/socket &
+fi
+
+
