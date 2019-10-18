1e7c56f0b558c6b9748931127459596cd8f7f56d
nisemono0 <dazzle.x00@gmail.com>
2019-10-19 01:51:13 +0300

    Added ranger command for archive viewing

--- .config/ranger/commands.py
+++ .config/ranger/commands.py
@@ -11,52 +11,41 @@ from __future__ import (absolute_import, division, print_function)
 
 # You can import any python module as needed.
 import os
+import shutil
 
 # You always need to import ranger.api.commands here to get the Command class:
 from ranger.api.commands import Command
+from ranger.core.loader import CommandLoader
 
+# Any class that is a subclass of "Command" will be integrated into ranger as a command.
 
-# Any class that is a subclass of "Command" will be integrated into ranger as a
-# command.  Try typing ":my_edit<ENTER>" in ranger!
-class my_edit(Command):
-    # The so-called doc-string of the class will be visible in the built-in
-    # help that is accessible by typing "?c" inside ranger.
-    """:my_edit <filename>
-
-    A sample command for demonstration purposes that opens a file in an editor.
-    """
-
-    # The execute method is called when you run this command in ranger.
+class archive_view(Command):
     def execute(self):
-        # self.arg(1) is the first (space-separated) argument to the function.
-        # This way you can write ":my_edit somefilename<ENTER>".
-        if self.arg(1):
-            # self.rest(1) contains self.arg(1) and everything that follows
-            target_filename = self.rest(1)
-        else:
-            # self.fm is a ranger.core.filemanager.FileManager object and gives
-            # you access to internals of ranger.
-            # self.fm.thisfile is a ranger.container.file.File object and is a
-            # reference to the currently selected file.
-            target_filename = self.fm.thisfile.path
+        tmp_folder = "/tmp/archive_view"
+        self._show(tmp_folder)
+        self._extract(self.fm.thisfile, tmp_folder)
+    
+    def _show(self, folder):
+        feh_flags = ['-T', 'normal']
+        descr = "Opening"
+        obj = CommandLoader(args=['feh'] + feh_flags + ["/tmp/archive_view"], descr=descr, read=False)
+        self.fm.loader.add(obj)
 
-        # This is a generic function to print text in ranger.
-        self.fm.notify("Let's edit the file " + target_filename + "!")
+    def _extract(self, archive, folder):
+        if not os.path.exists(folder): # If folder does not exist, create it
+            os.mkdir(folder)
+        else:                              # Otherwise delete w/e is inside
+            for the_file in os.listdir(folder):
+                file_path = os.path.join(folder, the_file)
+                try:
+                    if os.path.isfile(file_path):
+                        os.unlink(file_path)
+                    elif os.path.isdir(file_path): 
+                        shutil.rmtree(file_path)
+                except Exception as e:
+                    self.fm.notify(e)
 
-        # Using bad=True in fm.notify allows you to print error messages:
-        if not os.path.exists(target_filename):
-            self.fm.notify("The given file does not exist!", bad=True)
-            return
-
-        # This executes a function from ranger.core.acitons, a module with a
-        # variety of subroutines that can help you construct commands.
-        # Check out the source, or run "pydoc ranger.core.actions" for a list.
-        self.fm.edit_file(target_filename)
-
-    # The tab method is called when you press tab, and should return a list of
-    # suggestions that the user will tab through.
-    # tabnum is 1 for <TAB> and -1 for <S-TAB> by default
-    def tab(self, tabnum):
-        # This is a generic tab-completion function that iterates through the
-        # content of the current directory.
-        return self._tab_directory_content()
+        au_flags = ['-X', folder]
+        descr = "Extracting: " + os.path.basename(archive.path)
+        obj = CommandLoader(args=['aunpack'] + au_flags + [archive.path], descr=descr, read=True)
+        self.fm.loader.add(obj)
