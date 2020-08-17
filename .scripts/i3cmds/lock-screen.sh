e96baec835bb83cb68ccb91dfca71f5cdcb172eb
nisemono0 <dazzle.x00@gmail.com>
2020-08-17 19:21:14 +0300

    changed lockscreen

--- .scripts/i3cmds/lock-screen.sh
+++ .scripts/i3cmds/lock-screen.sh
@@ -1,14 +1,42 @@
-#!/bin/bash -e
+#!/usr/bin/bash -e
 
 TMPBG=/tmp/screen_locked.png
-
-# Take a screenshot
 scrot $TMPBG
 
-# Pixellate it 10x
+# Pixellate 10x
 mogrify -scale 10% -scale 1000% $TMPBG
 
-# Lock screen displaying this image.
-i3lock -i $TMPBG
+DATE=$(date +"%A, %B %-d")
+
+ringcolor=FFFFFFFF
+insidecolor=00000000
+linecolor=00000000
+separatorcolor=00000000
+
+ringvercolor=00000000
+insidevercolor=00000000
+
+ringwrongcolor=00000000
+insidewrongcolor=00000000
+
+keyhlcolor=AAAAAAFF
+bshlcolor=AAAAAAFF
+
+verifcolor=AAAAAAFF
+wrongcolor=DF2D2DFF
+greetercolor=AAAAAAFF
+
+layoutcolor=FFFFFFFF
+
+i3lock --image=$TMPBG  \
+    --pass-media-keys --keylayout 0 --layoutcolor=$layoutcolor \
+    --clock --force-clock --timepos="w/2:h/2" --timesize=96 --timecolor=FFFFFFFF \
+    --datestr="$DATE" --datepos="tx:ty+80" --datesize=32 --datecolor=FFFFFFFF \
+    --indpos="w/2-r/2:h-r*2" --radius 20 \
+    --ringcolor=$ringcolor --insidecolor=$insidecolor --linecolor=$linecolor \
+    --separatorcolor=$separatorcolor --ringvercolor=$ringvercolor \
+    --insidevercolor=$insidevercolor --veriftext='Verifying password' --verifcolor=$verifcolor --ringwrongcolor=$ringwrongcolor \
+    --insidewrongcolor=$insidewrongcolor --wrongtext='Wrong password' --wrongcolor=$wrongcolor --keyhlcolor=$keyhlcolor \
+    --bshlcolor=$bshlcolor --noinputtext='' --greetertext="Screen locked" --greetercolor=$greetercolor
 
 rm $TMPBG
