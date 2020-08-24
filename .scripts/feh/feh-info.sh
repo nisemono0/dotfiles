c78608b54f685576ba85a10e553c7bfeec56aff3
nisemono0 <dazzle.x00@gmail.com>
2020-08-24 16:49:44 +0300

    format

--- .scripts/feh/feh-info.sh
+++ .scripts/feh/feh-info.sh
@@ -6,5 +6,5 @@ width=$5
 height=$6
 zoom=$7
 
-printf "File Name: ${filename} [${filenr}/${totalfiles}] \nImage Size: ${imgsize}\nDimensions: ${width}x${height}\nZoom: ${zoom}"
+printf "File Name: ${filename} \nImage number:[${filenr}/${totalfiles}] \nImage Size: ${imgsize}\nDimensions: ${width}x${height}\nZoom: ${zoom}"
 
