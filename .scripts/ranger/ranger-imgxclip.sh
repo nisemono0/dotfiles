2e7769167456b5c9425a49cdad0022aebf918a4c
nisemono0 <dazzle.x00@gmail.com>
2020-09-02 01:47:51 +0300

    copy images to clipboard

---
+++ .scripts/ranger/ranger-imgxclip.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+filepath="$1"
+filetype=$(file --mime-type "$filepath" | cut -d ' ' -f 2)
+
+if [[ "$filetype" == image/* ]]; then
+	if [[ "$filetype" == image/png ]]; then
+		xclip -selection clipboard -target image/png -i "$filepath" && notify-send "Image copied to clipboard" || notify-send -u critical "Something went wrong"
+	else
+		convert "$filepath" png:- | xclip -selection clipboard -target image/png -i && notify-send "Image copied to clipboard" || notify-send -u critical "Something went wrong"
+	fi
+else
+	notify-send "Can only copy images into clipboard"
+fi
