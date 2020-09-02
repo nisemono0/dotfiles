4eebfc6f302326d39f732f80afbfaa06bda5a3a1
nisemono0 <dazzle.x00@gmail.com>
2020-09-02 06:11:50 +0300

    edit

--- .local/share/nemo/actions/extract-view.py
+++ .local/share/nemo/actions/extract-view.py
@@ -14,7 +14,7 @@ if len(argv) != 2:
 
 file_name = argv[1]
 
-def clear_tmp(folder):
+def clear_dir(folder):
     for the_file in os.listdir(folder):
         file_path = os.path.join(folder, the_file)
         try:
@@ -31,17 +31,7 @@ def extract(archive, folder):
     if not os.path.exists(folder): # If folder does not exist, create it
         os.mkdir(folder)
     else:                              # Otherwise delete w/e is inside
-        for the_file in os.listdir(folder):
-            file_path = os.path.join(folder, the_file)
-            try:
-                if os.path.isfile(file_path):
-                    os.unlink(file_path)
-                elif os.path.isdir(file_path): 
-                    rmtree(file_path)
-            except:
-                cmd = ["notify-send", "-u", "critical", "Something went wrong !"]
-                check_call(cmd)
-                exit(-1)
+        clear_dir(folder)
     try:
         cmd = ["notify-send", "Extracting..."]
         check_call(cmd)
@@ -63,4 +53,4 @@ def show(folder):
 
 extract(file_name, tmp_folder)
 show(tmp_folder)
-clear_tmp(tmp_folder)
+clear_dir(tmp_folder)
