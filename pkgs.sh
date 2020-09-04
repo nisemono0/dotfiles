c38a5f5ae668d3f8747fc0efe18a3be7f9f9bbdf
nisemono0 <dazzle.x00@gmail.com>
2020-09-04 04:53:38 +0300

    updatepkgs

--- pkgs.sh
+++ pkgs.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-$(pacman -Qqe > pkglist.txt)
+$(pacman -Qqe > allpkgs.txt)
+$(pacman -Qqet > mainpkgs.txt)
+$(pacman -Qqem > foreignpkgs.txt)
+$(comm -13 <(pacman -Qqdt | sort) <(pacman -Qqdtt | sort) > optdeplist.txt)
 $(pip freeze > piplist.txt)
