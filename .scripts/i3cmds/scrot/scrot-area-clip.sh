7335c37dd4dba1d454886db8a2b56725fc8b8d29
nisemono0 <dazzle.x00@gmail.com>
2020-09-11 14:01:18 +0300

    update scrot scripts

--- .scripts/i3cmds/scrot/scrot-area-clip.sh
+++ .scripts/i3cmds/scrot/scrot-area-clip.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/bash
-sleep 0.2 ; scrot -o -s -f /tmp/scrot-clip.png
-xclip -selection clipboard -target image/png -i /tmp/scrot-clip.png && notify-send "Screenshot saved in clipboard" || notify-send -u critical "Something went wrong"
-rm /tmp/scrot-clip.png
+sleep 0.2 ; (scrot -o -s -f /tmp/scrot-clip.png 2>/dev/null || exit) && \
+	xclip -selection clipboard -target image/png -i /tmp/scrot-clip.png && \
+   	notify-send "Screenshot saved in clipboard" && \
+    rm /tmp/scrot-clip.png || \
+   	(notify-send -u critical "Something went wrong" ; exit)
--- .scripts/i3cmds/scrot/scrot-clip.sh
+++ .scripts/i3cmds/scrot/scrot-clip.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
-scrot -o /tmp/scrot-clip.png 
-xclip -selection clipboard -target image/png -i /tmp/scrot-clip.png && notify-send "Screenshot saved in clipboard" || notify-send -u critical "Something went wrong"
-rm /tmp/scrot-clip.png
+(scrot -o /tmp/scrot-clip.png 2>/dev/null || exit) && \
+    xclip -selection clipboard -target image/png -i /tmp/scrot-clip.png && \
+    notify-send "Screenshot saved in clipboard" && \
+    rm /tmp/scrot-clip.png || \
+    (notify-send -u critical "Something went wrong" ; exit)
