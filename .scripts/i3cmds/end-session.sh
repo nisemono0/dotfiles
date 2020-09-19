1e6765866b6d5bb50c84bc946e4a077a5bb220ed
nisemono0 <dazzle.x00@gmail.com>
2020-09-19 03:04:09 +0300

    end session script

---
+++ .scripts/i3cmds/end-session.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+. $HOME/.dmenurc
+
+shut(){
+	sel=$(printf "Yes\\nNo" | dmenu -fn "$DMENU_FN" $DMENU_OPTIONS -i -p "Are you sure you want to shutdown ?") || exit 1
+	case "$sel" in
+		"Yes") shutdown ;;
+		*) exit ;;
+	esac
+}
+
+reb(){
+	sel=$(printf "Yes\\nNo" | dmenu -fn "$DMENU_FN" $DMENU_OPTIONS -i -p "Are you sure you want to reboot ?") || exit 1
+	case "$sel" in
+		"Yes") reboot ;;
+		*) exit ;;
+	esac
+}
+
+exit_i3(){
+	sel=$(printf "Yes\\nNo" | dmenu -fn "$DMENU_FN" $DMENU_OPTIONS -i -p "Are you sure you want to exit i3 ?") || exit 1
+	case "$sel" in
+		"Yes") i3-msg exit ;;
+		*) exit ;;
+	esac
+}
+
+selected=$(printf "Shutdown\\nReboot\\nExit i3" | dmenu -fn "$DMENU_FN" $DMENU_OPTIONS -i -p "End session") || exit 1
+case "$selected" in
+	"Shutdown") shut ;;
+	"Reboot") reb ;;
+	"Exit i3") exit_i3 ;;
+	*) exit ;;
+esac
