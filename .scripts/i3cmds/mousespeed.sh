520a5241f86be8fcce42e0de0387038907651e3d
nisemono0 <12276968+nisemono0@users.noreply.github.com>
2023-01-09 21:38:28 +0200

    change mouse accel profile

--- .scripts/i3cmds/mousespeed.sh
+++ .scripts/i3cmds/mousespeed.sh
@@ -2,38 +2,71 @@
 
 #xinput --list
 MOUSE_NAME=""
-PROP_NAME="libinput Accel Speed"
+PROP_NAME_SPEED="libinput Accel Speed"
+PROP_NAME_ACCEL="libinput Accel Profile Enabled"
 STEP=0.1
 
 [ $# -ne 1 ] && echo "Too few/many arguments, expecting 1" && exit 1
 
-speed=$(xinput --list-props "$MOUSE_NAME" | awk -F ':' -v PROP_NAME="$PROP_NAME" 'index($0, PROP_NAME) {print $2}' | head -n 1)
+speed=$(xinput --list-props "$MOUSE_NAME" | awk -F ':' -v PROP_NAME_SPEED="$PROP_NAME_SPEED" 'index($0, PROP_NAME_SPEED) {print $2}' | head -n 1 | tr -d "[:blank:]")
+accel=$(xinput --list-props "$MOUSE_NAME" | awk -F ':' -v PROP_NAME_ACCEL="$PROP_NAME_ACCEL" 'index($0, PROP_NAME_ACCEL) {print $2}' | head -n 1 | tr -d "[:blank:]")
 
 set_speed() {
-    if xinput --set-prop "$MOUSE_NAME" "$PROP_NAME" "$1"; then
-        notify-send -h string:x-dunst-stack-tag:mousespeed "Mouse speed changed" "Speed: $1"
+    if [ "$(bc <<< "$newspeed > 1")" = 1 ]; then
+        notify-send -h string:x-dunst-stack-tag:mouseopt "Mouse speed is at max" "Speed: $speed"
+        exit 1
+    elif [ "$(bc <<< "$newspeed < -1")" = 1 ]; then
+        notify-send -h string:x-dunst-stack-tag:mouseopt "Mouse speed is at min" "Speed: $speed"
+        exit 1
+    else
+        if xinput --set-prop "$MOUSE_NAME" "$PROP_NAME_SPEED" "$1"; then
+            notify-send -h string:x-dunst-stack-tag:mouseopt "Mouse speed changed" "Speed: $1"
+        else
+            notify-send -u critical "Couldn't set mouse speed"
+        fi
+    fi
+}
+
+reset_speed() {
+    if xinput --set-prop "$MOUSE_NAME" "$PROP_NAME_SPEED" 0.0; then
+        notify-send -h string:x-dunst-stack-tag:mouseopt "Mouse speed reset" "Speed: 0.0"
     else
         notify-send -u critical "Couldn't set mouse speed"
     fi
 }
 
-case "$1" in
-    -i|--inc) # Increase by STEP
-        newspeed=$(bc <<< "$speed + $STEP")
-        [ "$(bc <<< "$newspeed > 1")" = 1 ] && notify-send -h string:x-dunst-stack-tag:mousespeed "Mouse speed is at max" && exit 1
-        set_speed "$newspeed"
-        ;;
-    -d|--dec) # Decrease by STEP
-        newspeed=$(bc -l <<< "$speed - $STEP")
-        [ "$(bc <<< "$newspeed < -1")" = 1 ] && notify-send -h string:x-dunst-stack-tag:mousespeed "Mouse speed is at min" && exit 1
-        set_speed "$newspeed"
-        ;;
-    -r|--reset) # Reset
-        if xinput --set-prop "$MOUSE_NAME" "$PROP_NAME" 0.0; then
-            notify-send -h string:x-dunst-stack-tag:mousespeed "Mouse speed reset" "Speed: 0.0"
+toggle_accel() {
+    if [ "$accel" = "1,0" ]; then
+        if xinput --set-prop "$MOUSE_NAME" "$PROP_NAME_ACCEL" 0, 1; then
+            notify-send -h string:x-dunst-stack-tag:mouseopt "Mouse accel changed" "Accel: Flat (0, 1)"
         else
-            notify-send -u critical "Couldn't set mouse speed"
+            notify-send -u critical "Couldn't change mouse accel"
         fi
+    elif [ "$accel" = "0,1" ]; then
+        if xinput --set-prop "$MOUSE_NAME" "$PROP_NAME_ACCEL" 1, 0; then
+            notify-send -h string:x-dunst-stack-tag:mouseopt "Mouse accel changed" "Accel: Adaptive (1, 0)"
+        else
+            notify-send -u critical "Couldn't change mouse accel"
+        fi
+    else
+        notify-send -u critical "No mouse accel found"
+    fi
+}
+
+case "$1" in
+    --inc) # Increase by STEP
+        newspeed=$(bc <<< "$speed + $STEP")
+        set_speed "$newspeed"
+        ;;
+    --dec) # Decrease by STEP
+        newspeed=$(bc -l <<< "$speed - $STEP")
+        set_speed "$newspeed"
+        ;;
+    --accel) # Toggle mouse accel profile
+        toggle_accel
+        ;;
+    --reset) # Reset
+        reset_speed
         ;;
     *)
         echo "$0 <-i,--inc | -d,--dec | -r,--reset>"
