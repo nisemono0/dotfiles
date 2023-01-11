d7bbed688341a127a35cfa202fb8307e8baca6b5
nisemono0 <12276968+nisemono0@users.noreply.github.com>
2023-01-11 15:17:10 +0200

    update screencasting script with more options

--- .scripts/i3cmds/screencast.sh
+++ .scripts/i3cmds/screencast.sh
@@ -29,20 +29,70 @@ check_screencast() {
     fi
 }
 
+set_format() {
+    format=$(printf "webm\\nmp4\\nmkv" | dmenu -fn "$DMENU_FN" $DMENU_OPTIONS -p "Output format")
+    [ -z "${format}" ] && exit 1
+}
+
+set_framerate() {
+    framerate=$(printf "15\\n30\\n60" | dmenu -fn "$DMENU_FN" $DMENU_OPTIONS -p "Framerate")
+    [ -z "${framerate}" ] && exit 1
+}
+
+set_scale() {
+    case $(printf "No scale\\n900p\\n720p\\n540p\\n480p" | dmenu -fn "$DMENU_FN" $DMENU_OPTIONS -p "Video scale") in
+        "No scale") scale="-1:-1" ;;
+        "900p") scale="-1:900" ;;
+        "720p") scale="-1:720" ;;
+        "540p") scale="-1:540" ;;
+        "480p") scale="-1:480" ;;
+        *) exit ;;
+    esac
+}
+
+set_crf() {
+    crf=$(printf "Default" | dmenu -fn "$DMENU_FN" $DMENU_OPTIONS -p "Input CRF or use default (23)")
+    [ -z "${crf}" ] && exit 1
+    if [ "${crf}" = "Default" ]; then
+        crf="23"
+    fi
+}
+
+set_audio() {
+    case $(printf "Yes\\nNo" | dmenu -fn "$DMENU_FN" $DMENU_OPTIONS -p "Record audio") in
+        "Yes") audio="Yes" ;;
+        "No") audio="No" ;;
+        *) exit ;;
+    esac
+}
+
+set_preset() {
+    case $(printf "medium\\nfast\\nfaster\\nveryfast\\nsuperfast\\nultrafast\\nslow\\nslower\\nveryslow" | dmenu -fn "$DMENU_FN" $DMENU_OPTIONS -p "Preset") in
+        "medium") preset="medium" ;;
+        "fast") preset="fast" ;;
+        "faster") preset="faster" ;;
+        "veryfast") preset="veryfast" ;;
+        "superfast") preset="superfast" ;;
+        "ultrafast") preset="ultrafast" ;;
+        "slow") preset="slow" ;;
+        "slower") preset="slower" ;;
+        "veryslow") preset="veryslow" ;;
+        *) exit ;;
+    esac
+}
+
 start_screencast() {
     width="$1"
     height="$2"
     x="$3"
     y="$4"
 
-    format=$(printf "webm\\nmp4\\nmkv" | dmenu -fn "$DMENU_FN" $DMENU_OPTIONS -p "Output format")
-    [ -z "${format}" ] && exit 1
-
-    framerate=$(printf "15\\n30\\n60" | dmenu -fn "$DMENU_FN" $DMENU_OPTIONS -p "Framerate")
-    [ -z "${framerate}" ] && exit 1
-
-    audio=$(printf "Yes\\nNo" | dmenu -fn "$DMENU_FN" $DMENU_OPTIONS -p "Record audio")
-    [ -z "${audio}" ] && exit 1
+    set_format
+    set_framerate
+    set_scale
+    set_crf
+    set_preset
+    set_audio
 
     printf -v date "%(%F)T"
     printf -v time "%(%I-%M-%S)T"
@@ -53,9 +103,13 @@ start_screencast() {
 
     if [ "${audio}" = "Yes" ]; then
         sink="$(pactl info | sed -En 's/Default Sink: (.*)/\1/p').monitor"
-        ffmpeg -v 8 -y -f x11grab -video_size "${width}"x"${height}" -framerate "${framerate}" -i :0.0+"${x}","${y}" -f pulse -i "${sink}" "${output}" &
+        ffmpeg -v 8 -y -f pulse -i "${sink}" -f x11grab -video_size "${width}"x"${height}" \
+            -framerate "${framerate}" -i :0.0+"${x}","${y}" -preset "${preset}" \
+            -crf "${crf}" -vf scale="${scale}" "${output}" &
     else
-        ffmpeg -v 8 -y -f x11grab -video_size "${width}"x"${height}" -framerate "${framerate}" -i :0.0+"${x}","${y}" "${output}" &
+        ffmpeg -v 8 -y -f x11grab -video_size "${width}"x"${height}" \
+            -framerate "${framerate}" -i :0.0+"${x}","${y}" -preset "${preset}" \
+            -crf "${crf}" -vf scale="${scale}" "${output}" &
     fi
 
     notify-send -i video-display "Screencast started"
