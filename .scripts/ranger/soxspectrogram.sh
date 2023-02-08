b879ed1f5d607cbfd6b0df8fff0eb03a130a711d
nisemono0 <12276968+nisemono0@users.noreply.github.com>
2023-02-08 13:13:50 +0200

    rename and update some ranger scripts

--- .scripts/ranger/soxspectrogram.sh
+++ .scripts/ranger/soxspectrogram.sh
@@ -2,12 +2,12 @@
 
 filepath="$1"
 filetype=$(file --dereference --mime-type "$filepath" | cut -d ':' -f 2 | tr -d ' ')
-spectrogram="/tmp/spectrogram.png"
+spectrogram="$(mktemp)"
 
 if [[ "$filetype" == audio/* ]]; then
     sox "$filepath" -n spectrogram -o "$spectrogram"
     nsxiv "$spectrogram"
-    rm "$spectrogram"
+    rm -- "$spectrogram"
 else
 	notify-send "Can only view spectrals of audio files"
 fi
