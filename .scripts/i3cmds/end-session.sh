ae25f524d300fdea1b416f4ffc7f285abbeb28be
nisemono0 <12276968+nisemono0@users.noreply.github.com>
2023-05-01 04:38:27 +0300

    add sleep to ram option

--- .scripts/i3cmds/end-session.sh
+++ .scripts/i3cmds/end-session.sh
@@ -2,7 +2,7 @@
 . $HOME/.dmenurc
 
 shut(){
-	sel=$(printf "Yes\\nNo" | dmenu -fn "$DMENU_FN" $DMENU_OPTIONS -p "Are you sure you want to shutdown ?") || exit 1
+	sel=$(printf "Yes\\nNo" | dmenu -fn "$DMENU_FN" $DMENU_OPTIONS -p "Are you sure you want to shutdown") || exit 1
 	case "$sel" in
 		"Yes") shutdown now ;;
 		*) exit ;;
@@ -10,7 +10,15 @@ shut(){
 }
 
 reb(){
-	sel=$(printf "Yes\\nNo" | dmenu -fn "$DMENU_FN" $DMENU_OPTIONS -p "Are you sure you want to reboot ?") || exit 1
+	sel=$(printf "Yes\\nNo" | dmenu -fn "$DMENU_FN" $DMENU_OPTIONS -p "Are you sure you want to reboot") || exit 1
+	case "$sel" in
+		"Yes") reboot ;;
+		*) exit ;;
+	esac
+}
+
+sleep_toram(){
+	sel=$(printf "Yes\\nNo" | dmenu -fn "$DMENU_FN" $DMENU_OPTIONS -p "Are you sure you want to sleep") || exit 1
 	case "$sel" in
 		"Yes") reboot ;;
 		*) exit ;;
@@ -18,17 +26,18 @@ reb(){
 }
 
 exit_i3(){
-	sel=$(printf "Yes\\nNo" | dmenu -fn "$DMENU_FN" $DMENU_OPTIONS -p "Are you sure you want to logout ?") || exit 1
+	sel=$(printf "Yes\\nNo" | dmenu -fn "$DMENU_FN" $DMENU_OPTIONS -p "Are you sure you want to logout") || exit 1
 	case "$sel" in
 		"Yes") i3-msg exit ;;
 		*) exit ;;
 	esac
 }
 
-selected=$(printf "Shutdown\\nReboot\\nLogout" | dmenu -fn "$DMENU_FN" $DMENU_OPTIONS -p "End session") || exit 1
+selected=$(printf "Shutdown\\nReboot\\nSleep (RAM)\\nLogout" | dmenu -fn "$DMENU_FN" $DMENU_OPTIONS -p "End session") || exit 1
 case "$selected" in
 	"Shutdown") shut ;;
 	"Reboot") reb ;;
+    "Sleep (RAM)") sleep_toram ;;
 	"Logout") exit_i3 ;;
 	*) exit ;;
 esac
