b6416d98cf13b1a0adb47858777ce38b610bb8a2
nisemono0 <12276968+nisemono0@users.noreply.github.com>
2023-05-09 23:56:26 +0300

    now shows updates from aur packages

--- .scripts/polybar-scripts/update-arch.sh
+++ .scripts/polybar-scripts/update-arch.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
-if ! updates=$(checkupdates 2> /dev/null | wc -l ); then
-    updates=0
+if ! main_updates=$(checkupdates 2> /dev/null | wc -l ); then
+    main_updates=0
 fi
 
-if [ "$updates" -gt 0 ]; then
-    echo "# $updates"
+if ! aur_updates=$(yay -Qum 2> /dev/null | wc -l); then
+    aur_updates=0
+fi
+
+total_updates=$((main_updates + aur_updates))
+
+if [ "$total_updates" -gt 0 ]; then
+    echo "# $main_updates:$aur_updates"
 else
     echo ""
 fi
