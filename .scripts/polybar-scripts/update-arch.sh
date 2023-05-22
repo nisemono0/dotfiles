f156d1e3faf0601cb22a106dd1d25a7afb2f5f7f
nisemono0 <12276968+nisemono0@users.noreply.github.com>
2023-05-23 02:10:50 +0300

    show a detailed list of updates when clicking

--- .scripts/polybar-scripts/update-arch.sh
+++ .scripts/polybar-scripts/update-arch.sh
@@ -1,17 +1,44 @@
 #!/bin/sh
 
-if ! main_updates=$(checkupdates 2> /dev/null | wc -l ); then
-    main_updates=0
-fi
+total_updates(){
+    if ! main_updates=$(checkupdates 2> /dev/null | wc -l ); then
+        main_updates=0
+    fi
+    
+    if ! aur_updates=$(yay -Qum 2> /dev/null | wc -l); then
+        aur_updates=0
+    fi
+    
+    total_updates=$((main_updates + aur_updates))
+    
+    if [ "$total_updates" -gt 0 ]; then
+        echo "# $main_updates:$aur_updates"
+    else
+        echo ""
+    fi
+}
 
-if ! aur_updates=$(yay -Qum 2> /dev/null | wc -l); then
-    aur_updates=0
-fi
+detail_updates(){
+    echo "=====[Official]====="
+    if ! checkupdates; then
+        echo "No official updates"
+    fi
+    echo "=====[AUR]====="
+    if ! yay -Qum 2> /dev/null; then
+        echo "No AUR updates"
+    fi
 
-total_updates=$((main_updates + aur_updates))
+    read -rsn 1
+}
 
-if [ "$total_updates" -gt 0 ]; then
-    echo "# $main_updates:$aur_updates"
-else
-    echo ""
-fi
+case "$1" in
+    --total)
+        total_updates
+        ;;
+    --detail)
+        detail_updates
+        ;;
+    *)
+        exit 1
+        ;;
+esac
