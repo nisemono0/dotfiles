6ab512522d27f438543a694025cb104fd6008be0
nisemono0 <12276968+nisemono0@users.noreply.github.com>
2023-10-13 19:39:24 +0300

    add keybind to play a song from the current playlist add keybind to add and play a song from the database in the current playlist various fixes

--- .scripts/mpd/mpdsong.sh
+++ .scripts/mpd/mpdsong.sh
@@ -1,26 +1,50 @@
 #!/bin/bash
+. $HOME/.dmenurc
+
 SEEK_STEP=5
 
+play() {
+    if mpc "$1"; then
+        notify-send -h string:x-dunst-stack-tag:mpdsong "Now playing" "$(mpc current)"
+    else
+        notify-send -u critical "Couldn't play song"
+    fi
+}
+
+playsong() {
+    songpos=$(mpc playlist -f "%position%.[%artist%|%albumartist%|%file%] - %title%" | \
+        dmenu "${DMENU_ARGS[@]}" -fn "Noto Sans CJK JP:style=Bold:size=9" -l 15 -p "Play" | \
+        cut -f 1 -d '.')
+    [ -z "$songpos" ] && exit
+    if mpc play "$songpos"; then
+        notify-send -h string:x-dunst-stack-tag:mpdsong "Now playing" "$(mpc current)"
+    else
+        notify-send -u critical "Couldn't play song"
+    fi
+}
+
+addsongplay() {
+    songfile=$(mpc listall | dmenu "${DMENU_ARGS[@]}" -fn "Noto Sans CJK JP:style=Bold:size=9" -l 15 -p "Add & Play")
+    [ -z "$songfile" ] && exit
+    if mpc findadd filename "$songfile"; then
+        if mpc searchplay filename "$songfile"; then
+            notify-send -h string:x-dunst-stack-tag:mpdsong "Now playing" "$(mpc current)"
+        else
+            notify-send -u critical "Couldn't play song"
+        fi
+    else
+        notify-send -u critical "Couldn't change song"
+    fi
+}
+
 case "$1" in
-    --next) 
-        if mpc next; then
-            notify-send -h string:x-dunst-stack-tag:mpdsong "Now playing" "$(mpc current)"
-        else
-            nofiy-send -u critical "Couldn't change mpd song"
-        fi
-        ;;
-    --prev)
-        if mpc prev; then
-            notify-send -h string:x-dunst-stack-tag:mpdsong "Now playing" "$(mpc current)"
-        else
-            notif-send -u critical "Couldn't change mpd song"
-        fi
-        ;;
-    --forward)
-        mpc seek "+$SEEK_STEP"
-        ;;
-    --backward)
-        mpc seek "-$SEEK_STEP"
-        ;;
+    --play-next) play "next" ;; # Plays the next song in the playlist
+    --play-prev) play "prev" ;; # Plays the previous song in the playlist
+    --seek-forward) mpc seek "+$SEEK_STEP" ;; # Seeks forwards SEEK_STEP seconds
+    --seek-backward) mpc seek "-$SEEK_STEP" ;; # Seek backwards SEEK_STEP seconds
+    --play-song) playsong ;; # Select song to play from the playlist
+    --add-song-play) addsongplay ;; # Select song to add to the playlist and play it
+    --toggle-pause) mpc toggle ;; # Pause/Play toggle
+    --stop) mpc stop ;; # Stop playback
     *) exit ;;
 esac
