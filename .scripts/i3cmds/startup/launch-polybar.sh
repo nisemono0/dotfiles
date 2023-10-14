4a9768fc306b112c5015d2068583089fde794262
nisemono0 <12276968+nisemono0@users.noreply.github.com>
2023-10-14 16:14:12 +0300

    move each bar module into it's own file move bar colors in it's own file change bar name and some module color start polybar with the right bar name

--- .scripts/i3cmds/startup/launch-polybar.sh
+++ .scripts/i3cmds/startup/launch-polybar.sh
@@ -7,7 +7,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config.ini
-polybar -q top-bar &
+polybar -q top &
 
 echo "Polybar launched..."
 
