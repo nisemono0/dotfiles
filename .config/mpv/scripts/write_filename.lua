99bdc494e084f17d352f635717d9eb4c79904a91
nisemono0 <12276968+nisemono0@users.noreply.github.com>
2024-01-23 18:53:18 +0200

    add option to write to file when videofile starts only write to file if text doesn't exist

--- .config/mpv/scripts/write_filename.lua
+++ .config/mpv/scripts/write_filename.lua
@@ -6,6 +6,7 @@ local mpopts = require("mp.options")
 local opt = {
     out_dir = [[]],
     out_file = "watched.txt",
+    write_on_load = false,
     message_duration = 4
 }
 
@@ -22,22 +23,37 @@ function get_savefile()
     return opt.out_file
 end
 
+function text_exists(file, text)
+    for line in file:lines() do
+        if text == line then
+            return true
+        end
+    end
+    return false
+end
+
 function write_filename()
     save_file = get_savefile()
     playing_file = get_playing_file()
 
-    file = io.open(save_file, "a")
+    file = io.open(save_file, "a+")
     if file == nil then
         format_msg = string.format("Could not open: \"%s\"", save_file)
         mp.osd_message(format_msg, opt.message_duration)
         msg.error(format_msg)
     else
-        file:write(playing_file, "\n")
-        file:flush()
+        if not text_exists(file, playing_file) then
+            file:write(playing_file, "\n")
+            file:flush()
+            format_msg = string.format("Wrote \"%s\" to \"%s\"", playing_file, save_file)
+            mp.osd_message(format_msg, opt.message_duration)
+        end
         file:close()
-        format_msg = string.format("Wrote \"%s\" to \"%s\"", playing_file, save_file)
-        mp.osd_message(format_msg, opt.message_duration)
     end
+end
+
+if opt.write_on_load then
+    mp.register_event("file-loaded", write_filename)
 end
 
 mp.add_key_binding("w", "write_filename", write_filename)
