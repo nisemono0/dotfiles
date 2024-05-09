baa3a32cf1b5f37e87596f9a6cbd54e4a5667434
nisemono0 <12276968+nisemono0@users.noreply.github.com>
2024-05-09 19:17:44 +0300

    add brightness binds

---
+++ .scripts/i3cmds/backlight.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+CLASS="backlight"
+change_brightness(){
+    new_value="$(brightnessctl --class="$CLASS" set 10%$1 | grep -i "current brightness" | awk -F "[()]" '{ print $2 }')"
+    if [ -n "$new_value" ]; then
+        notify-send -h string:x-dunst-stack-tag:backlight "Brightness changed" "$new_value" -i video-display
+    else
+        notify-send -h string:x-dunst-stack-tag:backlight -u critical "Couldn't change brightness"
+    fi
+}
+
+case "$1" in
+    --inc)
+        change_brightness +
+        ;;
+    --dec)
+        change_brightness -
+        ;;
+    *)  exit ;;
+esac
