05fa0adbe5043c49b5f1f8ca1357919cef9d6f40
nisemono0 <12276968+nisemono0@users.noreply.github.com>
2024-08-21 20:11:48 +0300

    change dunst pause notification

--- .scripts/dunst/dunst-dnd.sh
+++ .scripts/dunst/dunst-dnd.sh
@@ -3,10 +3,10 @@
 TIMEOUT=5
 
 if [[ "$(dunstctl is-paused)" == "false" ]]; then
-    notify-send -h string:x-dunst-stack-tag:dunstdnd "Dunst" "Pause enabled"
+    notify-send -h string:x-dunst-stack-tag:dunstdnd "Dunst" "Paused"
     sleep "$TIMEOUT"
     dunstctl set-paused true
 else
     dunstctl set-paused false
-    notify-send -h string:x-dunst-stack-tag:dunstdnd "Dunst" "Pause disabled"
+    notify-send -h string:x-dunst-stack-tag:dunstdnd "Dunst" "Resumed"
 fi
