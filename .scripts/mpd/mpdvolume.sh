760c5b01ee651499acc080e796c41a29bc7ccd66
nisemono0 <12276968+nisemono0@users.noreply.github.com>
2024-12-11 21:12:44 +0200

    Prettify notification when changing mpd volume

--- .scripts/mpd/mpdvolume.sh
+++ .scripts/mpd/mpdvolume.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
+
 STEP=5
+DUNST_ICON="audio-volume-high-symbolic"
+VOLUME=$(mpc status '%volume%')
+VOLUME="${VOLUME//[^0-9]/}"
+
+update_vars() {
+    VOLUME=$(mpc status '%volume%')
+    VOLUME="${VOLUME//[^0-9]/}"
+    DUNST_ICON="audio-volume-muted-symbolic"
+    if (( VOLUME > 0 && VOLUME <= 35 )); then
+        DUNST_ICON="audio-volume-low-symbolic"
+    elif (( VOLUME > 35 && VOLUME <= 70 )); then
+        DUNST_ICON="audio-volume-medium-symbolic"
+    elif (( VOLUME > 70 && VOLUME <= 100 )); then
+        DUNST_ICON="audio-volume-high-symbolic"
+    fi
+}
 
 set_vol() {
     if mpc volume "$1"; then
-        new_volume=$(mpc status '%volume%')
-        new_volume="${new_volume//[^0-9]/}"
-        notify-send -i audio-volume-high-symbolic -h string:x-dunst-stack-tag:mpdvolume -h int:value:"$new_volume" "MPD Volume changed" "Volume: ${new_volume}%"
+        update_vars
+        notify-send -i "$DUNST_ICON" -h string:x-dunst-stack-tag:mpdvolume -h int:value:"$VOLUME" "MPD Volume" "Volume: ${VOLUME}%"
     else
         notify-send -u critical "Couldn't set mpd volume"
     fi
