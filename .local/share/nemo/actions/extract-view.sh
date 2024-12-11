2d6ce9ee9824d9d0e5609f80657cf721d5f1ce58
nisemono0 <12276968+nisemono0@users.noreply.github.com>
2024-12-11 19:19:51 +0200

    Show notification with progress bar when decompressing some archives types

--- .local/share/nemo/actions/extract-view.sh
+++ .local/share/nemo/actions/extract-view.sh
@@ -3,13 +3,39 @@
 archive="$1"
 tmpdir="$(mktemp -d)"
 filename="$(basename "$archive")"
+file_extension="${filename##*.}"
+file_extension="$(printf "%s" "${file_extension}" | tr '[:upper:]' '[:lower:]')"
 
-notify-send "Extracting..." "$filename" -i "package-x-generic"
+notify-send -i "package-x-generic" -h string:x-dunst-stack-tag:extractview "Extracting..." "$filename"
 
-if aunpack -X "$tmpdir" "$archive"; then
+view_extracted() {
     find "$tmpdir" -type f | sort -V | nsxiv -i -p
-else
-    notify-send -u critical "Error at extracting" "$archive"
-fi
+}
+
+extract_progress_bar() {
+    current_file=1
+    total_files="$1"
+    notif_bar_max=100
+    while IFS= read -r file; do
+        progress=$(( (notif_bar_max*current_file)/total_files ))
+        ((current_file++))
+        notify-send -i "package-x-generic" -h int:value:"$progress" -h string:x-dunst-stack-tag:extractview "Extracting..." "$filename"
+    done < <(aunpack -X "$tmpdir" "$archive")
+    view_extracted
+}
+
+extract_normal() {
+    if aunpack -X "$tmpdir" "$archive"; then
+        view_extracted
+    else
+        notify-send -u critical "Error at extracting" "$archive"
+    fi
+}
+
+case "${file_extension}" in
+    zip) extract_progress_bar $(zipinfo -1 "$archive" | wc -l) ;;
+    bz|bz2|gz|tar) extract_progress_bar $(tar --list --file "$1" | wc -l) ;;
+    *) extract_normal ;;
+esac
 
 rm -r -- "$tmpdir"
