6d328abe31ed798234ee79a0b83890d7a48d9327
nisemono0 <12276968+nisemono0@users.noreply.github.com>
2025-01-31 18:56:43 +0200

    Add action on notification update Add i3 rule for update window

--- .scripts/polybar/update-arch.sh
+++ .scripts/polybar/update-arch.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+TERM_CMD="/usr/bin/urxvt -geometry 100x25 -name notification-updates-arch -e sh -c"
+
 total_updates() {
     if ! main_updates=$(checkupdates 2> /dev/null | wc -l ); then
         main_updates=0
@@ -30,7 +32,7 @@ detail_updates() {
 
     printf "\n"
     echo "=========="
-    echo "Update now ? (y/n/a/q)"
+    echo "Update now ? (y[es]/n[o]/a[ll]/q[uit])"
     read -rsn 1 ans
     ans=$(echo "$ans" | tr '[:upper:]' '[:lower:]')
     case "$ans" in
@@ -66,7 +68,22 @@ notify_updates() {
         notify_text="${notify_text}===[AUR]==="$'\n'"No AUR updates"$'\n'
     fi
 
-    notify-send -t 0 -h string:x-dunst-stack-tag:sysupdates "System updates" "${notify_text}"
+    notif_action=$(notify-send -t 0 -h string:x-dunst-stack-tag:sysupdates \
+        -A "Main=Update official" -A "AUR=Update AUR" -A "All=Update all" \
+        "System updates" "${notify_text}")
+
+    case "$notif_action" in
+        "Main" )
+            $TERM_CMD "sudo pacman -Syu ; echo 'Press any key to exit' ; read -rsn 1"
+            ;;
+        "AUR")
+            $TERM_CMD "yay -Sua ; echo 'Press any key to exit' ; read -rsn 1"
+            ;;
+        "All")
+            $TERM_CMD "yay -Syu ; echo 'Press any key to exit' ; read -rsn 1"
+            ;;
+        *) exit ;;
+    esac
 }
 
 case "$1" in
