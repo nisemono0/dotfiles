7e725ef8c55d10e630e9d8493e342ac42718631c
nisemono0 <12276968+nisemono0@users.noreply.github.com>
2025-04-01 19:11:20 +0300

    Change foreignpkgs name to aurpkgs

--- pkgs/pkgs.sh
+++ pkgs/pkgs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pacman -Qqen | awk 'NR==FNR{a[$0];next} !($0 in a)' excludedpkgs.txt - > mainpkgs.txt
-pacman -Qqem | awk 'NR==FNR{a[$0];next} !($0 in a)' excludedpkgs.txt - > foreignpkgs.txt
+pacman -Qqem | awk 'NR==FNR{a[$0];next} !($0 in a)' excludedpkgs.txt - > aurpkgs.txt
 comm -13 <(pacman -Qqdt | sort) <(pacman -Qqdtt | sort) > optdeplist.txt
 #pip freeze > piplist.txt
