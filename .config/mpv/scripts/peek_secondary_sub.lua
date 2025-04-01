862006692a44c4dff4a9a028d050386e99fcf035
nisemono0 <12276968+nisemono0@users.noreply.github.com>
2025-04-01 22:21:25 +0300

    Update peek_secondary_sub script * Now has a keybind to also pause playback when peeking the secondary   sub * Update input.conf to add said keybind

--- .config/mpv/scripts/peek_secondary_sub.lua
+++ .config/mpv/scripts/peek_secondary_sub.lua
@@ -1,29 +1,48 @@
 local mp = require("mp")
 local msg = require("mp.msg")
 
-local was_held = false
+local was_pressed = nil
+local prev_pause_state = nil
 
-local function peek_secondary_sub(key)
+local function peek_secondary_sub(ev, pause_vid)
     local is_ssid_loaded = mp.get_property_native("secondary-sid")
-    local is_ssid_visible = mp.get_property_native("secondary-sub-visibility")
-
     if not is_ssid_loaded then
         return
     end
 
-    if key.event == "down" or key.event == "repeat" then
+    local is_ssid_visible = mp.get_property_native("secondary-sub-visibility")
+
+    if ev.event == "down" then
         if is_ssid_visible then
             return
         end
+
+        prev_pause_state = mp.get_property_native("pause")
+
+        if pause_vid then
+            mp.set_property_bool("pause", true)
+        end
+
         mp.set_property_bool("secondary-sub-visibility", true)
-        was_held = true
-    elseif key.event == "up" then
-        if was_held then
+        was_pressed = true
+    elseif ev.event == "up" then
+        if is_ssid_visible and was_pressed then
+            if pause_vid and prev_pause_state == false then
+                mp.set_property_bool("pause", false)
+            end
             mp.set_property_bool("secondary-sub-visibility", false)
-            was_held = false
+            was_pressed = false
         end
     end
 end
 
-mp.add_key_binding('Alt+V', 'peek-secondary-sub', peek_secondary_sub, { complex = true })
-mp.add_key_binding('k', 'peek-secondary-sub', peek_secondary_sub, { complex = true })
+local function handler_pause(ev)
+    peek_secondary_sub(ev, true)
+end
+
+local function handler_no_pause(ev)
+    peek_secondary_sub(ev, false)
+end
+
+mp.add_key_binding('k', 'peek-secondary-sub-no-pause', handler_no_pause, { complex = true })
+mp.add_key_binding('Alt+k', 'peek-secondary-sub-pause', handler_pause, { complex = true })
