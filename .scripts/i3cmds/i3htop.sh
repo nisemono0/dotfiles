b7fd7ebf35c649c6c85e650a208c04d29fe43ef6
nisemono0 <12276968+nisemono0@users.noreply.github.com>
2025-07-10 22:11:28 +0300

    Add keybinds to run htop/nvtop floating

--- .scripts/i3cmds/i3htop.sh
+++ .scripts/i3cmds/i3htop.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 # urxvt -e htop && i3-msg workspace back_and_forth
-alacritty --class 'Alacritty,htop-instance' --title 'htop' -e htop && i3-msg workspace back_and_forth
+
+case "$1" in
+    --tile) alacritty --class 'Alacritty,htop-instance-tile' --title 'htop' -e htop && i3-msg workspace back_and_forth ;;
+    --float) alacritty -o 'window.dimensions={columns=150, lines=40}' --class 'Alacritty,htop-instance-float' --title 'htop' -e htop ;;
+    *) exit 1 ;;
+esac
--- .scripts/i3cmds/i3nvtop.sh
+++ .scripts/i3cmds/i3nvtop.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 # urxvt -e nvtop && i3-msg workspace back_and_forth
-alacritty --class 'Alacritty,nvtop-instance' --title 'nvtop' -e nvtop && i3-msg workspace back_and_forth
+
+case "$1" in
+    --tile) alacritty --class 'Alacritty,nvtop-instance-tile' --title 'nvtop' -e nvtop && i3-msg workspace back_and_forth ;;
+    --float) alacritty -o 'window.dimensions={columns=150, lines=40}' --class 'Alacritty,nvtop-instance-float' --title 'nvtop' -e nvtop ;;
+    *) exit 1 ;;
+esac
