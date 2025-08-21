54ad180c6f0b10bfaf064ce87728ba2eef3dd901
nisemono0 <12276968+nisemono0@users.noreply.github.com>
2025-08-21 20:45:36 +0300

    Update script * Rename some paths for post install prints

--- install.sh
+++ install.sh
@@ -239,8 +239,8 @@ function post_install () {
     chsh -s /bin/zsh
     echo -e "${WH}Changed default shell for $USER to ${YE}zsh!${NC}"
 
-    to_modify_arr=("i3cmds/mousespeed.sh" "i3cmds/wall-change/wall-web.sh" "polybar/polybar-systemps.sh" "polybar/weather-forecast.sh" "polybar/weather.sh" "mpd/mpdsong.sh" "ncmpcpp/ncmpcpp_cover_art.sh")
-    echo -e "${WH}Please modify the following scripts, adding api keys, gpu/cpu names, custom paths, etc.:${NC}"
+    to_modify_arr=("wmcmds/wm-backlight/sh" "wmcmds/wm-toggle-comp.sh" "wmcmds/wm-mousespeed.sh" "wmcmds/wall-change/wall-web.sh" "polybar/polybar-systemps.sh" "polybar/weather-forecast.sh" "polybar/weather.sh" "mpd/mpdsong.sh" "ncmpcpp/ncmpcpp_cover_art.sh" "startup/monitor-setup.sh")
+    echo -e "${WH}Please modify the following scripts, add api keys, gpu/cpu names, custom paths, etc.:${NC}"
     for scr in "${to_modify_arr[@]}"; do
         echo -e "${PU}\t==>$HOME/.scripts/${scr}${NC}"
     done
