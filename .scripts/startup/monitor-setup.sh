b70a393b9dac70603ae84a54a7c95186c4501b9a
nisemono0 <12276968+nisemono0@users.noreply.github.com>
2025-08-26 22:45:53 +0300

    Update monitor setup

--- .scripts/startup/monitor-setup.sh
+++ .scripts/startup/monitor-setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 xrandr -r 144
-[[ -f ~/.config/monitor-profiles/BenQ_XL2411_Edited.icc ]] && xcalib -d :0 ~/.config/monitor-profiles/BenQ_XL2411_Edited.icc
+
+# [[ -f ~/.config/monitor-profiles/BenQ_XL2411_Edited.icc ]] && xcalib -d :0 ~/.config/monitor-profiles/BenQ_XL2411_Edited.icc
+
+[[ -f ~/.config/monitor-profiles/MSI_G244F_E2.icm ]] && xcalib -d :0 ~/.config/monitor-profiles/MSI_G244F_E2.icm
