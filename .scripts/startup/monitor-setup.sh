5b475910e01ad46180b101b63caf4a51eae63c28
nisemono0 <12276968+nisemono0@users.noreply.github.com>
2025-08-27 13:56:41 +0300

    Load icc file

--- .scripts/startup/monitor-setup.sh
+++ .scripts/startup/monitor-setup.sh
@@ -4,4 +4,4 @@ xrandr -r 180
 
 # [[ -f ~/.config/monitor-profiles/BenQ_XL2411_Edited.icc ]] && xcalib -d :0 ~/.config/monitor-profiles/BenQ_XL2411_Edited.icc
 
-[[ -f ~/.config/monitor-profiles/MSI_G244F_E2.icm ]] && xcalib -d :0 ~/.config/monitor-profiles/MSI_G244F_E2.icm
+[[ -f ~/.config/monitor-profiles/MSI_G244F_E2_Edited.icm ]] && xcalib -d :0 ~/.config/monitor-profiles/MSI_G244F_E2_Edited.icm
