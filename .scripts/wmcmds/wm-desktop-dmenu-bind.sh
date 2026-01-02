ade07bf6dd7a25dc9a8137525f4936a834c2b3bd
nisemono0 <12276968+nisemono0@users.noreply.github.com>
2026-01-02 20:22:27 +0200

    Add prompt name

--- .scripts/wmcmds/wm-desktop-dmenu-bind.sh
+++ .scripts/wmcmds/wm-desktop-dmenu-bind.sh
@@ -4,5 +4,5 @@
 # . $HOME/.dmenurc
 # echo "${DMENU_ARGS[@]}"
 
-i3-dmenu-desktop --dmenu='dmenu -b -i -nf "#8f525d" -nb "#0a0f18" -sf "#c1c3c5" -sb "#6c4853" -fn "SauceCodePro Nerd Font:style=Bold:size=10"'
+i3-dmenu-desktop --dmenu='dmenu -b -i -nf "#8f525d" -nb "#0a0f18" -sf "#c1c3c5" -sb "#6c4853" -fn "SauceCodePro Nerd Font:style=Bold:size=10" -p "Open"'
 
