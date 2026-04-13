027ecb2711381724024bdb51e72e0e10972353e0
nisemono0 <12276968+nisemono0@users.noreply.github.com>
2026-04-13 17:41:32 +0300

    Add yazi git plugin

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -93,3 +93,18 @@ require('spot'):setup {
   },
 }
 
+-- yazi-rs/plugins:git
+th.git = th.git or {}
+th.git.unknown_sign = ""
+th.git.modified_sign = "M"
+th.git.added_sign = "A"
+th.git.untracked_sign = "?"
+th.git.ignored_sign = "I"
+th.git.deleted_sign = "D"
+th.git.updated_sign = "U"
+th.git.clean_sign = " "
+require("git"):setup {
+    -- Order of status signs showing in the linemode
+    order = 1500,
+}
+
