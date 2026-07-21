ffc502d8ee783548550ede646980ee20af59eacc
nisemono0 <12276968+nisemono0@users.noreply.github.com>
2026-07-22 01:07:14 +0300

    Change lualine component separators

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -2,7 +2,7 @@ return {
     "nvim-lualine/lualine.nvim",
     opts = {
         options = {
-            component_separators = { left = "│", right = "│" },
+            component_separators = { left = ">", right = "<" },
             section_separators = { left = "", right = "" },
         },
     },
